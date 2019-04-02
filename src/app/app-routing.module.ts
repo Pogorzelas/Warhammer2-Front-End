df79370d5f99eaa9814ac3f441d87319ce4bbcfb
Tomassz <tomasz.pogorzelski@neoteric.eu>
2019-04-02 14:13:15 +0200

    delete: uninstall jquery, delete log and registration component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import {LogComponent} from '../components/log/log.component';
-import {NavigationBarComponent} from '../components/navigation-bar/navigation-bar.component';
-import {CharacterListComponent} from '../components/navigation-bar/character-list/character-list.component';
-import {RegistrationComponent} from '../components/log/registration/registration.component';
+import { NavigationBarComponent } from '../components/navigation-bar/navigation-bar.component';
+import { CharacterListComponent } from '../components/navigation-bar/character-list/character-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/log', pathMatch: 'full' },
-  { path: 'log', component: LogComponent },
-  { path: 'registration', component: RegistrationComponent },
+  { path: '', redirectTo: '/character_generator', pathMatch: 'full' },
   { path: 'character_generator', component: NavigationBarComponent },
   { path: 'character_list', component: CharacterListComponent }
 ];
